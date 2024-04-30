3f6b60f6a336d2ca12656e67929965f5f93f8854
Robert Wallner <robert.wallner.edward@gmail.com>
2024-04-30 09:29:12 +0000

    added color

--- gfx.c
+++ gfx.c
@@ -1,12 +1,14 @@
 #include <avr/pgmspace.h>
 #include "gfx.h"
-#include "ili9341.h"
-#include <string.h>
 
-void draw_text(uint16_t x, uint16_t y, const uint8_t * text, font_t * font, bool rtl) {
+long map(long x, long in_min, long in_max, long out_min, long out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void draw_text(uint16_t x, uint16_t y, const uint8_t * text, font_t * font, rgb888_t color, rgb888_t bgcolor, bool rtl) {
 	uint8_t bh = ((font->height - 1) / 8 + 1) * 2;
 	int16_t offset = 0;
-	for (int chx = 0; chx < strlen((char *)text); chx++) {
+	for (int chx = 0; chx < strlen(text); chx++) {
 		uint8_t chr = text[chx] - font->offset;
 		uint16_t char_offset = 0;
 		for (int i = 0; i < chr; i++)
@@ -18,20 +20,13 @@ void draw_text(uint16_t x, uint16_t y, const uint8_t * text, font_t * font, bool
 				//uint8_t val = font->data[char_offset + bh * rcl + b];
 				for (int p = 0; p < 4; p++) {
 					uint8_t v = (val >> (2 * p)) & 0b11;
-					switch(v) {
-						case 0:
-							ili9341_set_pixel(x + offset, y + 4 * b + p, 0x0000);
-							break;
-						case 1:
-							ili9341_set_pixel(x + offset, y + 4 * b + p, 0x6800);
-							break;
-						case 2:
-							ili9341_set_pixel(x + offset, y + 4 * b + p, 0x7800);
-							break;
-						case 3:
-							ili9341_set_pixel(x + offset, y + 4 * b + p, 0xF800);
-							break;
-					}
+					int ratio = ((uint8_t[]){0, 50, 70, 100})[v];
+					rgb888_t rgb;
+					rgb.red = map(ratio, 0, 100, bgcolor.red, color.red);
+					rgb.green = map(ratio, 0, 100, bgcolor.green, color.green);
+					rgb.blue = map(ratio, 0, 100, bgcolor.blue, color.blue);
+					uint16_t col = RGB565(rgb);
+					ili9341_set_pixel(x + offset, y + 4 * b + p, col);
 				}
 			}
 			if (rtl)
@@ -44,19 +39,19 @@ void draw_text(uint16_t x, uint16_t y, const uint8_t * text, font_t * font, bool
 
 uint16_t text_width(uint8_t * text, font_t * font) {
 	uint16_t width = 0;
-	for (int c = 0; c < strlen((char *)text); c++)
+	for (int c = 0; c < strlen(text); c++)
 		width += font->widths[text[c] - font->offset];
 	return width;
 }
 
-void draw_text_box(uint16_t x, uint16_t y, uint16_t width, uint16_t h, uint8_t * text, font_t * font, align_t align, bool rtl) {
+void draw_text_box(uint16_t x, uint16_t y, uint16_t width, uint16_t h, uint8_t * text, font_t * font, rgb888_t color, rgb888_t bgcolor, align_t align, bool rtl) {
 	uint16_t pad = 0;
 	if (align == AlignRight)
 		pad = width - text_width(text, font);
 	else if (align == AlignCenter)
 		pad = (width - text_width(text, font)) / 2;
 	if (rtl)
-		draw_text(width - x - pad, y, text, font, rtl);
+		draw_text(width - x - pad, y, text, font, color, bgcolor, rtl);
 	else
-		draw_text(x + pad, y, text, font, rtl);
+		draw_text(x + pad, y, text, font, color, bgcolor, rtl);
 }
--- gfx.h
+++ gfx.h
@@ -17,12 +17,43 @@ typedef enum {
 	AlignCenter
 } align_t;
 
+typedef union {
+	uint8_t value[3];
+	struct {
+		uint8_t red;
+		uint8_t green;
+		uint8_t blue;
+	};
+} rgb888_t;
+
+#define Red (rgb888_t){255, 0, 0}
+#define Orange (rgb888_t){255, 125, 0}
+#define Yellow (rgb888_t){255, 255, 0}
+#define Green (rgb888_t){0, 255, 0}
+#define SpringGreen (rgb888_t){125, 255, 0}
+#define Turquoise (rgb888_t){0, 255, 125}
+#define Cyan (rgb888_t){0, 255, 255}
+#define Ocean (rgb888_t){0, 125, 255}
+#define Blue (rgb888_t){0, 0, 255}
+#define Violet (rgb888_t){125, 0, 255}
+#define Magenta (rgb888_t){255, 0, 255}
+#define Raspberry (rgb888_t){255, 0, 125}
+
+#define White (rgb888_t){255, 255, 255}
+#define LightGray (rgb888_t){0xD3, 0xD3, 0xD3}
+#define Silver (rgb888_t){0xC0, 0xC0, 0xC0}
+#define Gray (rgb888_t){0x80, 0x80, 0x80}
+#define DarkGray (rgb888_t){0xA9, 0xA9, 0xA9}
+#define Black (rgb888_t){0, 0, 0}
+
+#define RGB565(c) (((c.red >> 3) << 11) | ((c.green >> 2) << 5) | c.blue >> 3)
+
 #ifdef __cplusplus
 extern "C" {
 #endif
-void draw_text(uint16_t x, uint16_t y, const uint8_t * text, font_t * font, bool rtl);
 uint16_t text_width(uint8_t * text, font_t * font);
-void draw_text_box(uint16_t x, uint16_t y, uint16_t width, uint16_t h, uint8_t * text, font_t * font, align_t align, bool rtl);
+void draw_text(uint16_t x, uint16_t y, const uint8_t * text, font_t * font, rgb888_t color, rgb888_t bgcolor, bool rtl);
+void draw_text_box(uint16_t x, uint16_t y, uint16_t width, uint16_t h, uint8_t * text, font_t * font, rgb888_t color, rgb888_t bgcolor, align_t align, bool rtl);
 #ifdef __cplusplus
 }
 #endif
