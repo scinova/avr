edfad11c16426d616a6ce847d80ad530740f0651
Robert Wallner <robert.wallner.edward@gmail.com>
2024-05-03 18:30:57 +0000

    rx functions

--- uart.c
+++ uart.c
@@ -8,38 +8,37 @@ static uint8_t tx_buffer[UART_TX_BUFFER_SIZE + 1];
 static volatile uint8_t tx_buffer_head;
 static volatile uint8_t tx_buffer_tail;
 
-static volatile uint8_t rx_buffer[UART_RX_BUFFER_SIZE + 1];
-static volatile uint8_t rx_buffer_head;
-static volatile uint8_t rx_buffer_tail;
+static uart_handler_t _handler;
 
-void _uart_enable(uint32_t bitrate, bool enable_tx, bool enable_rx) {
+void _uart_enable(uint32_t bitrate, bool enable_rx) {
 	// SEL0=0 UPM0=0 USBS0=0 UCSZ0=3 async, no parity, 1 stop bit, 8 bit size
 	UCSR0C |= _BV(UCSZ00) | _BV(UCSZ01);
 	uint32_t b = (F_CPU / (bitrate * 16UL)) - 1;
 	UBRR0L = b;
 	UBRR0H = b >> 8;
-	//if (enable_tx)
-	//	UCSR0B |= _BV(TXEN0);
 	if (enable_rx)
 		UCSR0B |= _BV(RXEN0) | _BV(RXCIE0);
 	sei();
 }
 
-void uart_enable(uint32_t bitrate) {
-	_uart_enable(bitrate, true, true);
+void uart_enable(uint32_t bitrate, uart_handler_t handler) {
+	_handler = handler;
+	_uart_enable(bitrate, true);
 }
 
 void uart_enable_tx_only(uint32_t bitrate) {
-	_uart_enable(bitrate, true, false);
+	_uart_enable(bitrate, false);
 }
 
-void uart_enable_rx_only(uint32_t bitrate) {
-	_uart_enable(bitrate, false, true);
+void uart_enable_rx_only(uint32_t bitrate, uart_handler_t handler) {
+	_handler = handler;
+	_uart_enable(bitrate, true);
 }
 
 void uart_disable() {
 	UCSR0B = 0;
 	UCSR0C = 0;
+	_handler = NULL;
 }
 
 bool uart_tx_buffer_is_empty() {
@@ -72,20 +71,6 @@ ISR(USART_UDRE_vect) {
 		tx_buffer_tail = next;
 	} else
 		UCSR0B &= ~_BV(TXEN0);
-		//UCSR0B &= ~_BV(UDRIE0);
-}
-
-bool uart_data_is_available() {
-	return (rx_buffer_head != rx_buffer_tail);
-}
-
-uint8_t uart_read() {
-		if (rx_buffer_head == rx_buffer_tail)
-			return 0;
-		uint8_t next = (rx_buffer_tail < UART_RX_BUFFER_SIZE - 1 ? rx_buffer_tail + 1 : 0);
-		uint8_t ch = rx_buffer[rx_buffer_tail];
-		rx_buffer_tail = next;
-		return ch;
 }
 
 #if defined (__AVR_ATmega2560__)
@@ -94,9 +79,6 @@ ISR(USART0_RX_vect) {
 ISR(USART_RX_vect) {
 #endif
 	uint8_t ch = UDR0;
-	uint8_t next = (rx_buffer_head < UART_RX_BUFFER_SIZE - 1 ? rx_buffer_head + 1 : 0);
-	if (next == rx_buffer_tail)
-		return;
-	rx_buffer[rx_buffer_head] = ch;
-	rx_buffer_head = next;
+	if (_handler)
+		_handler(ch);
 }
--- uart.h
+++ uart.h
@@ -4,26 +4,21 @@
 #include <stdbool.h>
 
 #if !defined(UART_TX_BUFFER_SIZE)
-#define UART_TX_BUFFER_SIZE 50
+#define UART_TX_BUFFER_SIZE 500
 #endif
-#if !defined(UART_RX_BUFFER_SIZE)
-#define UART_RX_BUFFER_SIZE 50
-#endif
+
+typedef void(*uart_handler_t)(uint8_t ch);
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-void uart_enable(uint32_t bitrate);
+void uart_enable(uint32_t bitrate, uart_handler_t handler);
 void uart_enable_tx_only(uint32_t bitrate);
-void uart_enable_rx_only(uint32_t bitrate);
+void uart_enable_rx_only(uint32_t bitrate, uart_handler_t handler);
 void uart_disable();
-
 bool uart_tx_buffer_is_empty();
 void uart_write(uint8_t * data, uint8_t len);
 void uart_print(uint8_t * str);
-
-bool uart_data_is_available();
-uint8_t uart_read();
 #ifdef __cplusplus
 }
 #endif
